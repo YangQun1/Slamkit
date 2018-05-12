c161f94aa6c8a78151e0eb85a8abf7df253c3241
YangQun <1921296014@qq.com>
2018-05-12 22:10:48 +0800

    初步实现基于RGB-D的SLAM
    实现了使用相邻两帧的数据,计算相机的位姿,并构建环境的点云地图的功能

--- src/main.cpp
+++ src/main.cpp
@@ -12,39 +12,57 @@
 #include "detect_features.h"
 #include "align_image.h"
 #include "param_reader.h"
+#include "image_reader.h"
 
 using namespace std;
 
 int main(int argc, char**argv)
 {
-	int status;
-	Frame frame1, frame2;
+	int start_index, end_index;
 	
-	// 实例化
 	ParameterReader param_reader("./param.txt");
-	
+	ImageReader image_reader(&param_reader);
 	PinHoleCamera cam( &param_reader );
 	DetectFeatures detector(&param_reader);
 	AlignImage align_image(&cam,&param_reader);
 	PointCloudGenerator point_cloud_generator(&cam, &param_reader);
-	// 读取图像
-	frame1.rgbImg_ = cv::imread( "../data/rgb1.png" );
-	frame1.depthImg_ = cv::imread( "../data/depth1.png", -1 );
-	frame2.rgbImg_ = cv::imread( "../data/rgb2.png" );
-	frame2.depthImg_ = cv::imread( "../data/depth2.png", -1 );
 	
-	// 设置第一帧的坐标系为世界坐标系
-	frame1.T_f_w_ = Sophus::SE3(Eigen::Matrix3d::Identity(), Eigen::Vector3d::Zero());
+	start_index = param_reader.getParam<int>("start_index");
+	end_index = param_reader.getParam<int>("end_index");
 	
-	// 检测特征
-	detector.detectFeaturePoints(frame1);
-	detector.detectFeaturePoints(frame2);
+	// 构造第一帧
+	FramePtr ref_frame, curr_frame;
+	image_reader.readImage(start_index);
+	ref_frame.reset( new Frame( &cam, image_reader.getRGBImg().clone(), image_reader.getDepthImg().clone() ) );
+	ref_frame->T_c2w_ = Sophus::SE3(Eigen::Matrix3d::Identity(), Eigen::Vector3d::Zero());	// 设置第一帧的坐标系到世界坐标系的变换为单位阵
 	
-	// 匹配,计算变换关系
-	align_image.alignImage(frame1, frame2);
+	// 提取第一帧的特征
+	detector.detectFeaturePoints(ref_frame);
+	// 生成第一帧的点云
+	point_cloud_generator.joinPointCloud(ref_frame);
 	
-	// 合并点云并显示
- 	point_cloud_generator.joinPointCloud(frame1, frame2);
+	// 循环处理其他帧
+	for(int index = start_index + 1; index <= end_index; index++) {
+		// 构造当前帧
+		image_reader.readImage(index);
+		curr_frame.reset( new Frame( &cam, image_reader.getRGBImg().clone(), image_reader.getDepthImg().clone() ) ); 
+		
+		// 提取当前帧的特征
+		detector.detectFeaturePoints(curr_frame);
+		
+		// 匹配,计算变换关系
+		align_image.alignImage(ref_frame, curr_frame);
+		
+		if (true == align_image.checkAlignQuality()) {
+			// 合并点云并显示
+			point_cloud_generator.joinPointCloud(curr_frame);
+			ref_frame = curr_frame;
+		}
+		curr_frame.reset();
+	}
+	
+	// 保存最终的点云文件
+	point_cloud_generator.savePointCloud();
 	
 	return 0;
 }
